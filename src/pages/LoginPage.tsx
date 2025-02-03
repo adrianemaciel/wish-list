85b501ccf5c00ea306b84f315a051949a182ae3e
Adriane Maciel <adrianemaciel.developer@gmail.com>
2025-02-03 17:30:19 -0300

    style: enhance Login Page UI with improved layout and background image

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -14,25 +14,30 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="max-w-[1216px] mx-auto py-5 flex flex-col items-center">
-        <h1 className="text-3xl font-bold mb-4">Login</h1>
-        <select
-          className="border p-2 rounded mb-4"
-          value={role}
-          onChange={(e) => setRole(e.target.value)}
-        >
-          <option value="">Selecione seu perfil</option>
-          <option value="admin">Aniversariante</option>
-          <option value="guest">Convidado</option>
-        </select>
-        <button
-          className="bg-blue-500 text-white p-2 rounded"
-          onClick={handleLogin}
-        >
-          Entrar
-        </button>
-      </div>
+    <div className="flex items-center min-h-screen bg-[url(https://images.unsplash.com/photo-1513151233558-d860c5398176?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-center bg-cover bg-no-repeat h-[100vh] w-screen ">
+      <form className="max-w-sm mx-auto">
+        <div className=" py-5 flex flex-col items-center justify-center rounded-lg shadow-md bg-cyan-100 h-96 w-96">
+          <h1 className="text-3xl font-bold mb-4">Bem vindo(a)!</h1>
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">
+            Escolha seu perfil para continuar
+          </label>
+          <select
+            className="border-b border-gray-300 p-2 w-50 mb-4 focus:outline-none focus:border-b-2 focus:border-blue-500"
+            value={role}
+            onChange={(e) => setRole(e.target.value)}
+          >
+            <option selected>Selecione seu perfil</option>
+            <option value="admin">Aniversariante</option>
+            <option value="guest">Convidado</option>
+          </select>
+          <button
+            className="bg-cyan-950 text-white w-45 p-2 rounded hover:bg-cyan-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+            onClick={handleLogin}
+          >
+            Entrar
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
