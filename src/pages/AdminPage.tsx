27a0cb397a7bbbc699d95cf4c6f632b82105a8f2
Adriane Maciel <adrianemaciel.developer@gmail.com>
2025-02-04 16:48:49 -0300

    style: enhance AdminPage header with highlighted "Desejos" text

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -4,7 +4,9 @@ import ButtonBackToLogin from "../components/buttons/ButtonBackToLogin";
 const AdminPage = () => {
   return (
     <main className="max-w-[1216px] mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Gerenciar Lista de Desejos</h1>
+      <h1 className="text-2xl font-bold mb-4">
+        Gerenciar Lista de <span className="text-yellow-500">Desejos</span>
+      </h1>
 
       <ButtonBackToLogin />
 
